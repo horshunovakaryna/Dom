fc07470964c5de7a3ee48a7408b90737da4379fc
KarynaKhorshunova <horshunova.karyna@gmail.com>
2018-09-26 13:23:56 +0300

    solution of task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-alert("hello");
+
+let money = prompt("Сколько у вас с собой есть денег?");
+let countApple = prompt("Сколько вы купили яблок?");
+let countBread = prompt("Сколько вы купили батонов хлеба?");
+let costOfApple = +prompt("Сколько стоит одно яблоко?") * + countApple;
+let costOfBread = +prompt("Сколько стоит один батон хлеба?") * +countBread;
+if(money > costOfApple+costOfBread) alert(true);
+else alert(false);
