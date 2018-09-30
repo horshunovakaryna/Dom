f5548c5b0fe4bc485dd4a2583c22c3db5e9dff47
KarynaKhorshunova <horshunova.karyna@gmail.com>
2018-09-30 13:56:57 +0300

    New task

--- js/script.js
+++ js/script.js
@@ -1,13 +1,2 @@
-let answer1 = prompt("Введите свой ход");
-let answer2 = prompt("Введите свой ход");
-
-if(answer1 == "scissors" && (answer2 == "paper" || answer2 == "rock")) {
-    (answer2 == "paper") ? alert("User1 winner") : alert("User2 winner");
-} else if(answer1 == "rock" && (answer2 == "paper" || answer2 == "scissors")) {
-    (answer2 == "paper") ? alert("User2 winner") : alert("User1 winner");
-} else if(answer1 == "paper" && (answer2 == "scissors" || answer2 == "rock")){
-    (answer2 == "scissors") ? alert("User1 winner") : alert("User1 winner");
-} else if(answer1 == answer2){
-    answer1 = prompt("Введите свой ход");
-    answer2 = prompt("Введите свой ход");
-}
+let numb = prompt("Введите число");
+(numb >= 0)? alert(numb):alert(-numb);
