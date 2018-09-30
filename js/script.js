015ff4d8966a76677e8288dfb49edc98e4215511
KarynaKhorshunova <horshunova.karyna@gmail.com>
2018-09-30 13:51:34 +0300

    code of game

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
+let answer1 = prompt("Введите свой ход");
+let answer2 = prompt("Введите свой ход");
 
-const numb = prompt("Введите число");
-if(numb >= 0) alert(numb);
-else alert(-numb);
+if(answer1 == "scissors" && (answer2 == "paper" || answer2 == "rock")) {
+    (answer2 == "paper") ? alert("User1 winner") : alert("User2 winner");
+} else if(answer1 == "rock" && (answer2 == "paper" || answer2 == "scissors")) {
+    (answer2 == "paper") ? alert("User2 winner") : alert("User1 winner");
+} else if(answer1 == "paper" && (answer2 == "scissors" || answer2 == "rock")){
+    (answer2 == "scissors") ? alert("User1 winner") : alert("User1 winner");
+} else if(answer1 == answer2){
+    answer1 = prompt("Введите свой ход");
+    answer2 = prompt("Введите свой ход");
+}
